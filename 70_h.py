1a7c50a44b328733bce7887b2915d5e2a7d97557
chc <99154196+ch77ancreant@users.noreply.github.com>
2022-08-14 16:10:28 +0800

    create 70_h.py

---
+++ 70_h.py
@@ -0,0 +1,24 @@
+def binom(n, k):
+    return math.factorial(n) // math.factorial(k) // math.factorial(n - k)
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+          
+        i = 1
+        n_ways = 0
+        
+        if n%2 == 1:
+            while n-i > i:
+                n_ways += binom(n-i, i)
+                i += 1
+            return n_ways+1
+            
+        elif n%2 == 0:
+            while n-i > i:
+                n_ways += binom(n-i, i)
+                i += 1
+            return n_ways+2
